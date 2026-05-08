b4a381103180bec8f8eb61f5795105c5bdf6d5e2
Diego Vicente-Sastre <80335147+BioChess@users.noreply.github.com>
2026-05-08 14:38:27 +0200

    Refactor interactive map code for clarity and updates

--- 01_Interactive_map.R
+++ 01_Interactive_map.R
@@ -22,8 +22,8 @@ pal.colors <- colorFactor(palette = "Set1", domain = gps.df$birdID)
 # Crear el mapa base
 leafletOptions <- leaflet::leafletOptions(preferCanvas = TRUE)
 imap <- leaflet(options = leafletOptions) %>%
-  addTiles(options = tileOptions(maxZoom = 10))
-
+  addTiles(options = tileOptions(maxZoom = 10)) %>%
+  addProviderTiles("Esri.WorldImagery", group = "Satélite")
 # Agregar capas de tracks por cada individuo
 grupos <- unique(gps.df$birdID)  # Obtener los ID únicos
 
